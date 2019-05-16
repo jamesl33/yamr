bf8191be92f65b54780ebe5a194f70a561e12de8
James Lee <jamesl33info@gmail.com>
2019-05-16 17:23:03 +0100

    Added unit testing for each media type to help avoid regressions

---
+++ test/test_movie.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""
+This file is part of yamr "Yet Another Media Renamer".
+
+Copyright (C) 2019, James Lee <jamesl33info@gmail.com>.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import os
+
+from unittest import mock
+
+from yamr.cli import yamr
+
+
+def test_rename_movie(tmp_path):
+    (tmp_path / '28.Days.Later.2002.1080p.mkv').touch()
+
+    YAMR = yamr.YAMR({'folder': tmp_path, 'dry_run': False}, {})
+
+    with mock.patch('builtins.input', side_effect='1'):
+        YAMR.rename_media_files()
+
+    files = [os.path.basename(f) for f in tmp_path.iterdir()]
+
+    assert '28 Days Later... (2002).mkv' in files
+
+
+def test_multiple_movie_detection(tmp_path):
+    (tmp_path / '28.Days.Later.2002.1080p.mkv').touch()
+    (tmp_path / '28.Weeks.Later.2007.1080p.mkv').touch()
+
+    YAMR = yamr.YAMR({'folder': tmp_path, 'dry_run': False}, {})
+
+    with mock.patch('builtins.input', side_effect=['1', '1']):
+        YAMR.rename_media_files()
+
+    files = [os.path.basename(f) for f in tmp_path.iterdir()]
+
+    assert '28 Days Later... (2002).mkv' in files
+    assert '28 Weeks Later (2007).mkv' in files
